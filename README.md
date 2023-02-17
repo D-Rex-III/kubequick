0b701346a4a4763a1f81784adbb4ca09403820f2
Dave Rex <drex@idevops.io>
2023-02-16 23:10:41 -0700

    added functionality

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # kubequick
-A bash script to list and use Kubernetes pods in your cluster.
+A bash script to list and use Kubernetes pods in your cluster. But quicker.
 
 
 ## What happens when I run this script?
-1. Running the script will prompt you to enter a namespace or hit enter for default namespace.
-2. The script will look through the namespace and return the pods with an associated number. 
+1. Running the script will prompt you to enter a namespace or enter 'all' to get all pods in all namespaces. You can also use the default namespace by simply hitting enter on the first prompt.
+2. The script will look through the selected namespace and return the pods with an associated number. 
 3. Enter the number of the pod you want to use and hit enter.
-4. The next prompt will ask if you want to "exec, logs, or describe" the pod. Type out one of the selections and hit enter.
+4. The next prompt will ask if you want to "exec, logs, or describe" the pod. Type out one of the selections and hit enter for the action entered.
--- kubequick.sh
+++ kubequick.sh
@@ -1,48 +1,64 @@
 #!/bin/bash
 
-echo "Enter namespace (Press enter for default namespace): "
-read namespace
+echo "Enter namespace (Enter 'all' for all namespaces): "
+read -r -e -p "" namespace
 
 if [ -z "$namespace" ]; then
     namespace="default"
+    namespaces=("$namespace")
+    NAMESPACE_FLAG="-n $namespace"
+    pods=($(kubectl get pods $NAMESPACE_FLAG 2>/dev/null | awk '{print $1}' | tail -n +2))
+elif [ "$namespace" = "all" ]; then
+    NAMESPACE_FLAG="--all-namespaces"
+    namespaces=($(kubectl get pods $NAMESPACE_FLAG 2>/dev/null | awk '{print $1}' | tail -n +2))
+    pods=($(kubectl get pods $NAMESPACE_FLAG 2>/dev/null | awk '{print $2}' | tail -n +2))
+else
+    NAMESPACE_FLAG="-n $namespace"
+    namespaces=("$namespace")
+    pods=($(kubectl get pods $NAMESPACE_FLAG 2>/dev/null | awk '{print $1}' | tail -n +2))
 fi
-
-pods=($(kubectl get pods -n $namespace 2>/dev/null | awk '{print $1}' | tail -n +2))
 
 if [ ${#pods[@]} -eq 0 ]; then
-    echo "No pods found in namespace $namespace"
+    echo "Can not find namespace: $namespace"
     exit 1
 fi
 
-for i in "${!pods[@]}"; do
-    echo "$i: ${pods[i]}"
+if [ "$namespace" = "all" ]; then
+    for name_count in "${!pods[@]}"; do
+        echo "$name_count: ${namespaces[$name_count]}/${pods[$name_count]}"
+    done
+else
+    for name_count in "${!pods[@]}"; do
+        echo "$name_count: ${namespace}/${pods[$name_count]}"
+    done
+fi
+
+while true; do
+    read -r -p "Enter the number of the pod you want to select: " pod_num
+    if [[ ! $pod_num =~ ^[0-9]+$ ]] || [ "$pod_num" -lt 0 ] || [ "$pod_num" -ge ${#pods[@]} ]; then
+        echo "Invalid input. Please enter a valid number."
+    else
+        break
+    fi
 done
 
-echo "Enter the number of the pod you want to select: "
-read pod_num
-
-if [ $pod_num -lt 0 ] || [ $pod_num -ge ${#pods[@]} ]; then
-    echo "Invalid input. Please enter a valid number."
-    exit 1
-fi
-
+selected_namespace=${namespaces[pod_num]}
 selected_pod=${pods[pod_num]}
 
-echo "What would you like to do with the selected pod? (exec, logs, describe)"
-read action
-
-if [ $action == "exec" ]; then
-    echo "Enter the shell to exec into the pod (sh or bash): "
-    read shell
-
-    if [ -z "$shell" ]; then
-        shell="sh"
+while true; do
+    read -r -p "Choose action for pod: $selected_pod? (exec, logs, describe): " action
+    if [ "$action" == "exec" ]; then
+        read -r -p "Enter the shell to exec into the pod [sh or bash]: " shell
+        shell=${shell:-sh}
+        kubectl -n "$selected_namespace" exec -it "$selected_pod" -- "$shell" || echo "Error executing kubectl exec command."
+        break
+    elif [ "$action" == "logs" ]; then
+        kubectl -n "$selected_namespace" logs "$selected_pod" || echo "Error executing kubectl logs command."
+        break
+    elif [ "$action" == "describe" ]; then
+        kubectl -n "$selected_namespace" describe pod "$selected_pod" || echo "Error executing kubectl describe command."
+        break
+    else
+        echo "Invalid action, please enter a valid action (exec, logs, describe)."
     fi
-    kubectl -n $namespace exec -it $selected_pod -- $shell
-elif [ $action == "logs" ]; then
-    kubectl -n $namespace logs $selected_pod
-elif [ $action == "describe" ]; then
-    kubectl -n $namespace describe pod $selected_pod
-else
-    echo "Invalid action, please enter a valid action (exec, logs, describe)"
-fi
+done
